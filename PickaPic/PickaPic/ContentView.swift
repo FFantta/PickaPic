1fabaf61674223f4386a68087cfda15d4944eb23
Junrun Chen <junrunchen@JunrundeMacBook-Pro.local>
2024-12-04 01:39:12 +0000

    feat: light bottom icon

--- PickaPic/PickaPic/ContentView.swift
+++ PickaPic/PickaPic/ContentView.swift
@@ -8,29 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab = 0
+
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             TodayView()
                 .tabItem {
                     VStack {
-                        Image("bottom1_d")
+                        Image(selectedTab == 0 ? "icon1_l" : "bottom1_d")
                             .resizable()
                             .scaledToFit()
                             .frame(width: 10, height: 10)
                         // Text("今日")
                     }
                 }
+                .tag(0)
             
             HistoryView()
                 .tabItem {
                     VStack {
-                        Image("bottom2_d")
+                        Image(selectedTab == 1 ? "icon2_l" : "bottom2_d")
                             .resizable()
                             .scaledToFit()
                             .frame(width: 10, height: 10)
                         // Text("记录")
                     }
                 }
+                .tag(1)
         }
     }
 }
